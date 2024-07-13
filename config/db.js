3443a9a105fca9fca83e90cca36be609538a0e88
Rahul sahani <153098636+Rahulsahani18@users.noreply.github.com>
2024-07-13 22:12:07 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,14 +1,20 @@
-import mongoose from "mongoose";
-import colors from "colors";
 const connectDB = async () => {
   try {
-    const conn = mongoose.connect(process.env.MONGO_URL);
-    console.log(
-      `Conneted To Mongodb Databse ${conn.connection}`.bgMagenta.white
-    );
-  } catch (error) {
-    console.log(`Errro in Mongodb ${error}`.bgRed.white);
-  }
-}; 
+    await mongoose.connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
 
-export default connectDB;
+    const dbConnection = mongoose.connection;
+
+    dbConnection.on('connected', () => {
+      console.log('Connected to MongoDB!');
+    });
+
+    dbConnection.on('error', (err) => {
+      console.error('Error connecting to MongoDB:', err);
+    });
+  } catch (error) {
+    console.error('Error in MongoDB connection:', error);
+  }
+};
